234794b61f73b08d64119dbb1aa32bd48f03fdf5
EryanaRadzi <eryanamohdradzi@gmail.com>
2019-01-04 16:30:54 +0800

    Change to Yana

--- TestingGitHub3/Program.cs
+++ TestingGitHub3/Program.cs
@@ -9,7 +9,7 @@ namespace TestingGitHub3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("zita");
+            Console.WriteLine("yana");
         }
     }
 }
